5c585d5f18132f18119ce25447418566c942c364
Nils Jacobsen <nils.jacobsen98@gmail.com>
2025-12-10 13:52:56 +0100

    initial

---
+++ index.ts
@@ -0,0 +1,9 @@
+import { openLegitFsWithMemoryFs } from '@legit-sdk/core/server';
+
+async function main() {
+  const legitFs = await openLegitFsWithMemoryFs();
+  const dir = await legitFs.promises.readdir('/', 'utf8');
+  console.log(dir);
+}
+
+main();
